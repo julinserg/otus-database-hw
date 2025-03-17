9d9b437962964729471131e4d11ceecab034e3f4
julin.serg <julin.serg@gmail.com>
2025-03-17 21:25:51 +0300

    hw02 fix

--- hw02_indexes/shop_add_indexes.sql
+++ hw02_indexes/shop_add_indexes.sql
@@ -1,64 +1,70 @@
 -- категория продуктов(ID, название категории)
 CREATE TABLE ProductCategory (
     Id INT PRIMARY KEY,
-    Name TEXT
+    Name TEXT NOT NULL
 );
 
 -- производитель продуктов(ID, название, контактные данные)
 CREATE TABLE Producer (
     Id INT PRIMARY KEY,
-    Name TEXT,
-    Email TEXT,
-    Address TEXT,
-    ContactNumber TEXT
+    Name TEXT NOT NULL,
+    Email TEXT NOT NULL,
+    Address TEXT NOT NULL,
+    ContactNumber TEXT NOT NULL
 );
 
 -- поставщик продуктов(ID, название, контактные данные)
 CREATE TABLE Provider (
     Id INT PRIMARY KEY,
-    Name TEXT,
-    Email TEXT,
-    Address TEXT,
-    ContactNumber TEXT
+    Name TEXT NOT NULL,
+    Email TEXT NOT NULL,
+    Address TEXT NOT NULL,
+    ContactNumber TEXT NOT NULL
 );
 
 -- продукт(ID, название, описание, цена, категория, производитель)
--- CategoryID связь один-ко-многим, так как один продукт может быть 
--- только в одной конкретнйо категории (продукты питания, товары для дома, автотовары)
 -- ProducerID связь один-ко-многим, так как один продукт может быть
 -- произвден только один конкретным производителем
 CREATE TABLE Product (
     Id INT PRIMARY KEY,
-    Name TEXT,
-    Description TEXT,
-    Price INT,
-    CategoryID INT references ProductCategory(Id),
+    Name TEXT NOT NULL,
+    Description TEXT NOT NULL,
+    Price INT NOT NULL,
     ProducerID INT references Producer(Id)
 );
 
 -- связь поставщика продукта с продуктом многие-ко-многим, так как 
 -- один и тот же продукт может поставляться разными поставщиками и 
--- соответсвенно разные поставщики могут поставлять один и тот же продукт
+-- соответственно разные поставщики могут поставлять один и тот же продукт
 CREATE TABLE ProviderToProduct (
     ProductID INT references Product(Id),
     ProviderID INT references Provider(Id),
     PRIMARY KEY(ProductID, ProviderID)
 );
 
+-- связь категории продукта с продуктом многие-ко-многим, так как 
+-- один и тот же продукт может входить в разные категории и 
+-- соответственно разные категории могут содержать один и тот же продукт
+CREATE TABLE CategoryToProduct (
+    ProductID INT references Product(Id),
+    CategoryID INT references ProductCategory(Id),
+    PRIMARY KEY(ProductID, CategoryID)
+);
+
 -- покупатель (ID, имя, контактные данные)
 CREATE TABLE Customer (
     Id INT PRIMARY KEY,
-    Name TEXT, 
-    Email TEXT,
-    Address TEXT,
-    ContactNumber TEXT
+    Name TEXT NOT NULL, 
+    Email TEXT NOT NULL,
+    Address TEXT NOT NULL,
+    ContactNumber TEXT NOT NULL
 );
 
 -- заказ (ID заказа, ID покупателя, дата оформления заказа)
 CREATE TABLE ShopOrder (
     Id INT PRIMARY KEY,
     CustomerID INT references Customer(Id),
-    OrderDate TIMESTAMP
+    OrderDate TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 -- элемент заказа (ID, ID заказа, ID продукта, количество, итоговая цена)
@@ -68,8 +74,37 @@ CREATE TABLE ShopOrderItem (
     Id INT PRIMARY KEY,
     OrderID INT references ShopOrder(Id),
     ProductID INT references Product(Id),
-    Count INT,
-    Price INT
+    Count INT NOT NULL,
+    Price INT NOT NULL
 );
 
+-- ИНДЕКСЫ
+
+-- индекс для фильтра/сортировки продуктов по цене
+CREATE INDEX product_price_idx ON Product(Price);
+
+-- индекс для фильтра/сортировки продуктов по производителям (и дополнительно по диапазону цен)
+-- цена указана последней в индексе несмотря на то, что обладает большей кардинальностью
+-- это обсуловлено поиском по диапазону цен, а не по конкретному значению
+CREATE INDEX product_producer_price_idx ON Product(ProducerID, Price);
+
+-- индекс для полтотекстового поиска продуктов по названию
+CREATE INDEX product_name_en_idx ON Product USING GIN(to_tsvector('english', Name));
+CREATE INDEX product_name_ru_idx ON Product USING GIN(to_tsvector('russian', Name));
+
+-- индекс для фильтра продуктов по категориям
+-- фактически это обратный индекс к индексу первичного ключа (ProductID, CategoryID)
+CREATE INDEX categorytoproduct_category_idx ON CategoryToProduct(CategoryID);
+
+-- индекс для фильтра продуктов по поставщикам
+-- фактически это обратный индекс к индексу первичного ключа (ProductID, ProviderID)
+CREATE INDEX providertoproduct_provider_idx ON ProviderToProduct(ProviderID);
+
+-- индекс для фильтра/сортировки заказов по дате
+CREATE INDEX shoporder_orderdate_idx ON ShopOrder(OrderDate);
+
+-- индекс для фильтра заказов по покупателю (и дополнительно по диапазону дат)
+-- дата указана последней в индексе несмотря на то, что обладает большей кардинальностью
+-- это обсуловлено поиском по диапазону дат, а не по конкретному значению
+CREATE INDEX shoporder_сustomer_orderdate_idx ON ShopOrder(CustomerID, OrderDate);
 
